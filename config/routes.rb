4ccefa59724d6871ece094cf1ce416db262b6a66
dynakashima <nakashima.dy@gmail.com>
2014-12-13 08:53:39 -0800

    created static Pages controller along with views for home about and contact us

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+  get 'about' => 'static_pages#about'
+  get 'contactUs' => 'static_pages#contactUs'
+  # get 'static_pages/home'
+  # get 'static_pages/about'
+  # get 'static_pages/contactUs'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
